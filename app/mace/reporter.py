f216c2ee4f9d0a05d97f27284829d27b785d2ea1
lescipi <pietrolesci@outlook.com>
2020-04-25 19:33:18 +0200

    mace_reporter template

--- app/mace/reporter.py
+++ app/mace/reporter.py
@@ -2,7 +2,35 @@
 # This class is in charge of taking attributes
 # from the Mace fitted object and turn it into
 # an Excel file to be written on disk
+from pandas import DataFrame, ExcelWriter
+from pathlib import Path
+from app.mace.mace import Mace
+from app.config import UPLOAD_FOLDER
 
 
 class MaceReporter(object):
-    pass
+    """
+    Create excel file report to send to user.
+    """
+
+    def __init__(self, data: Mace, filename: str):
+
+        self.data = {
+
+            "first_sheet": DataFrame({}),
+
+            "second_sheet": DataFrame({})
+
+        }
+
+        self.destination = Path(__file__).parent / \
+            UPLOAD_FOLDER / f"attachment_{filename}.xslx"
+
+    def save(self) -> None:
+        """
+        Method to save the algorithm results to an Excel file.
+        It expects sheet names and pandas DataFrames.
+        """
+        with ExcelWriter(self.destination) as file_writer:
+            for sheet_name, dataframe in self.data.items():
+                dataframe.to_excel(file_writer, sheet_name, index=False)
